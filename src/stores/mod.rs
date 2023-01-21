82fe43acdb040fbf5243a91d449d5c2a3ad4a5fa
Tamo <tamo@meilisearch.com>
2023-01-21 15:48:11 +0100

    finish a first ugly implementation of the sql thingy

--- src/stores/mod.rs
+++ src/stores/mod.rs
@@ -1,3 +1,6 @@
+use core::fmt;
+use std::str::FromStr;
+
 use serde::{Deserialize, Serialize};
 
 pub mod custom;
@@ -19,6 +22,7 @@ pub enum Type {
     IndexDeletion,
     IndexSwap,
     DocumentAddition,
+    DocumentDeletion,
 }
 
 #[derive(Debug, Serialize, Deserialize, Copy, Clone)]
@@ -32,12 +36,64 @@ pub struct Task {
 pub struct Query {
     task_id: Option<Vec<TaskId>>,
 
-    statuses: Option<Vec<Status>>,
-    types: Option<Vec<Type>>,
-
     after_id: Option<TaskId>,
     before_id: Option<TaskId>,
 
+    statuses: Option<Vec<Status>>,
+    types: Option<Vec<Type>>,
+
     limit: usize,
     offset: usize,
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Status::Enqueued => write!(f, "enqueued"),
+            Status::Processing => write!(f, "processing"),
+            Status::Succeeded => write!(f, "succeeded"),
+            Status::Failed => write!(f, "failed"),
+        }
+    }
+}
+
+impl FromStr for Status {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "enqueued" => Ok(Status::Enqueued),
+            "processing" => Ok(Status::Processing),
+            "succeeded" => Ok(Status::Succeeded),
+            "failed" => Ok(Status::Failed),
+            s => panic!("received unknow string while deserializing a Status: {s:?}."),
+        }
+    }
+}
+
+impl fmt::Display for Type {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Type::IndexCreation => write!(f, "indexCreation"),
+            Type::IndexDeletion => write!(f, "indexDeletion"),
+            Type::IndexSwap => write!(f, "indexSwap"),
+            Type::DocumentAddition => write!(f, "documentAddition"),
+            Type::DocumentDeletion => write!(f, "documentDeletion"),
+        }
+    }
+}
+
+impl FromStr for Type {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "indexCreation" => Ok(Type::IndexCreation),
+            "indexDeletion" => Ok(Type::IndexDeletion),
+            "indexSwap" => Ok(Type::IndexSwap),
+            "documentAddition" => Ok(Type::DocumentAddition),
+            "documentDeletion" => Ok(Type::DocumentDeletion),
+            s => panic!("received unknow string while deserializing a Type: {s:?}."),
+        }
+    }
 }
--- src/stores/sqlite.rs
+++ src/stores/sqlite.rs
@@ -1,7 +1,13 @@
 use anyhow::Result;
-use rusqlite::Connection;
+use rusqlite::{
+    params,
+    types::{FromSql, FromSqlError, FromSqlResult, ToSqlOutput, ValueRef},
+    Connection, ToSql,
+};
+use std::fmt::Write;
+use std::str::FromStr;
 
-use super::Task;
+use super::{Query, Status, Task, Type};
 
 pub struct SqliteStore {
     com: Connection,
@@ -9,22 +15,153 @@ pub struct SqliteStore {
 
 impl SqliteStore {
     pub fn new() -> Self {
-        SqliteStore {
-            com: Connection::open("store.sqlite").unwrap(),
-        }
+        let connection = Connection::open("store.sqlite").unwrap();
+        connection
+            .prepare(
+                r#"
+                DROP TABLE IF EXISTS tasks;
+                DROP TYPE IF EXISTS status;
+                DROP TYPE IF EXISTS type;
+
+                CREATE TYPE status AS ENUM ('enqueued', 'processing', 'succeeded', 'failed');
+                CREATE TYPE type AS ENUM ('indexCreation', 'indexDeletion', 'indexSwap', 'documentAddition', 'documentDeletion');
+                
+                CREATE TABLE IF NOT EXISTS tasks (
+                    task_id INT PRIMARY KEY,
+                    status status,
+                    type type
+        );
+        "#,
+            )
+            .expect("Error while preparing init query")
+            .raw_execute()
+            .expect("Error while executing init query");
+
+        SqliteStore { com: connection }
     }
 
     pub fn register(&self, task: Task) -> Result<()> {
-        let mut doc_stmt = self
-            .com
+        self.com
             .prepare(
                 r#"
-            INSERT INTO documents (doc_id, document) VALUES (?, ?)
-            ON CONFLICT(doc_id) DO UPDATE SET document = excluded.document;
+            INSERT INTO tasks (task_id, status, type) VALUES (?, ?, ?);
             "#,
             )
-            .unwrap();
+            .unwrap()
+            .execute(params![task.id, task.status, task.r#type])
+            .expect("Error while inserting document");
 
         Ok(())
+    }
+
+    pub fn query(&self, query: Query) -> Result<Vec<Task>> {
+        let mut request = format!(
+            "SELECT (task_id, status, type) FROM tasks LIMIT {} OFFSET {} WHERE",
+            query.limit, query.offset
+        );
+
+        if let Some(task_ids) = query.task_id {
+            write!(
+                request,
+                " task_id IN [{}]",
+                task_ids
+                    .iter()
+                    .map(|id| id.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )
+            .unwrap();
+        }
+
+        if let Some(after_id) = query.after_id {
+            write!(request, " task_id > {after_id}").unwrap();
+        }
+
+        if let Some(before_id) = query.before_id {
+            write!(request, " task_id < {before_id}").unwrap();
+        }
+
+        if let Some(statuses) = query.statuses {
+            write!(
+                request,
+                " status IN [{}]",
+                statuses
+                    .iter()
+                    .map(|id| id.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )
+            .unwrap();
+        }
+
+        if let Some(types) = query.types {
+            write!(
+                request,
+                " type IN [{}]",
+                types
+                    .iter()
+                    .map(|id| id.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )
+            .unwrap();
+        }
+
+        request.push_str(";");
+
+        Ok(self
+            .com
+            .prepare(&request)
+            .unwrap()
+            .query_map(params![], |row| {
+                Ok(Task {
+                    id: row.get(0)?,
+                    status: row.get(1)?,
+                    r#type: row.get(2)?,
+                })
+            })
+            .unwrap()
+            .map(Result::unwrap)
+            .collect())
+    }
+}
+
+impl ToSql for Status {
+    fn to_sql(&self) -> rusqlite::Result<ToSqlOutput<'_>> {
+        Ok(self.to_string().into())
+    }
+}
+
+impl FromSql for Status {
+    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+        match value {
+            rusqlite::types::ValueRef::Text(s) => {
+                Ok(Self::from_str(std::str::from_utf8(s).unwrap()).unwrap()) // .map_err(|_| FromSqlError::Other)
+            }
+            rusqlite::types::ValueRef::Integer(_)
+            | rusqlite::types::ValueRef::Real(_)
+            | rusqlite::types::ValueRef::Blob(_)
+            | rusqlite::types::ValueRef::Null => Err(FromSqlError::InvalidType),
+        }
+    }
+}
+
+impl ToSql for Type {
+    fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
+        Ok(serde_json::to_string(&self).unwrap().into())
+    }
+}
+
+impl FromSql for Type {
+    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+        match value {
+            rusqlite::types::ValueRef::Text(s) => {
+                Ok(Self::from_str(std::str::from_utf8(s).unwrap()).unwrap()) //.map_err(|_| FromSqlError::Other)
+            }
+            rusqlite::types::ValueRef::Integer(_)
+            | rusqlite::types::ValueRef::Real(_)
+            | rusqlite::types::ValueRef::Blob(_)
+            | rusqlite::types::ValueRef::Null => Err(FromSqlError::InvalidType),
+        }
     }
 }
